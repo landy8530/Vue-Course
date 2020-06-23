75200b89e984e9c51c15c20aaeab8915b41a10ab
landyl <Landy.Liu@ehealth.com>
2020-06-23 16:01:13 +0800

    Vue 组件

---
+++ base-input.js
@@ -0,0 +1,32 @@
+Vue.component('base-input', {
+  inheritAttrs: false,
+  props: ['label', 'value'],
+  computed: {
+    inputListeners: function () {
+      var vm = this
+      // `Object.assign` 将所有的对象合并为一个新对象
+      return Object.assign({},
+        // 我们从父级添加所有的监听器
+        this.$listeners,
+        // 然后我们添加自定义监听器，
+        // 或覆写一些监听器的行为
+        {
+          // 这里确保组件配合 `v-model` 的工作
+          input: function (event) {
+            vm.$emit('input', event.target.value)
+          }
+        }
+      )
+    }
+  },
+  template: `
+    <label>
+      {{ label }}
+      <input
+        v-bind="$attrs"
+        v-bind:value="value"
+        v-on="inputListeners"
+      >
+    </label>
+  `
+})
---
+++ emit-component.js
@@ -0,0 +1,27 @@
+Vue.component('welcome-button', {
+  template: `
+    <button v-on:click="$emit('welcome')">
+      Click me to be welcomed
+    </button>
+  `
+})
+
+//配合额外的参数使用 $emit：
+Vue.component('magic-eight-ball', {
+  data: function () {
+    return {
+      possibleAdvice: ['Yes', 'No', 'Maybe']
+    }
+  },
+  methods: {
+    giveAdvice: function () {
+      var randomAdviceIndex = Math.floor(Math.random() * this.possibleAdvice.length)
+      this.$emit('give-advice', this.possibleAdvice[randomAdviceIndex])
+    }
+  },
+  template: `
+    <button v-on:click="giveAdvice">
+      Click me for advice
+    </button>
+  `
+})
